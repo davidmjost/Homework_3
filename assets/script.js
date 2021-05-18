6b1e3e5b5db5ddc8a7f12a4bb5013891c9dfeff2
David Jost <davidmjost1@gmail.com>
2021-05-17 21:56:29 -0400

    Final Commit

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+
+
+
 var pLength = prompt ("Password length? (Must be between 8 and 120 characters)", "");
 if (pLength < 8 || ask > 120 ){
       alert("Length must be between 8-120 characters")
@@ -10,17 +13,17 @@ var numbers = confirm("Include numbers?");
 var symbols = confirm("Include symbols?");
 };
 
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector(".generate");
 
 var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var symbols = "!@#$%^&*_-+=";
 
-// var passwordText = document.getElementById("password");
-// var length = document.getElementById("length");
-// var incNumbers = document.getElementById("numbers");
-// var incSymbols = document.getElementById("symbols");
-// var generateBtn = document.getElementById("generate");
+var passwordText = document.getElementById("password");
+var length = document.getElementById("length");
+var incNumbers = document.getElementById("numbers");
+var incSymbols = document.getElementById("symbols");
+var generateBtn = document.getElementById("generate");
 
 
 // Write password to the #password input
@@ -33,11 +36,11 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword); {
-//   var characters = alpha;
+generateBtn.addEventListener("click", writePassword); {
+  var characters = alpha;
   
-//   passwordTxt.value = generatePassword(length.value, characters);
-// };
+  passwordTxt.value = generatePassword(length.value, characters);
+};
 
 function generatePassword (length, characters); {
   var password = "";
